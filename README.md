ff3202fffe25e616655c34a389e4d66c9cd6f2ea
seattlenomnom <mark.crapser@gmail.com>
2025-03-10 21:04:08 -0700

    Worked char and ints into sd42

--- README.md
+++ README.md
@@ -10,3 +10,13 @@ SD42 is a "Hello, world!" program in cpp
 03/10/2025 try some formated output. just add to another stream.
 
 03/10/2025 add underlinging to column headings.
+
+03/10/2025 move to data types, don't create a new project. create a char
+variable, define it, put on output stream, redefine it, put on output stream.
+
+
+Mon 10 Mar 2025 08:53:19 PM PDT this should say Mon 10 Mar 2025...
+
+
+Mon 10 Mar 2025 08:54:29 PM PDT
+experiment with int types.
--- sd42.cpp
+++ sd42.cpp
@@ -7,9 +7,29 @@
 
 
 int main() {
+
+    char c = 'a';
+    int x = 123;
+    int y = 33;
+
+
+
     std::cout << "Hello, World!\n" << "This is anothere string\n";
 
     std::cout << "column 1 heading\tcolumn 2 heading\n";
 
     std::cout << "----------------\t----------------\n";
+
+    std::cout << "The character is : " << c << "\n";
+
+    std::cout << "The size of a character is: " << sizeof(char) << " bytes\n";
+
+    std::cout << "The value of x is: " << x << "\n";
+
+    std::cout << "The value of y is: " << y << "\n";
+
+    std::cout << "The size of an int is " << sizeof(int) << "\n";
+
+
+
 }
